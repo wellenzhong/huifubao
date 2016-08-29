5eef8a800837b39bfcbe9f41f1a5cdcd86b1a975
welllen zhong <wellen2000@qq.com>
2016-08-29 17:36:47 +0800

    create new
    新创建的项目

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+export default React.createClass({
+	render: function() {
+		return (
+			<div>大家好！</div>
+		);
+	}
+})
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import Hello from 'App.js'
+React.render(
+	<Hello />
+	,document.getElementById('mainBody'))
